df32e6b255c21120a72a6741b127bc1d111790c1
Dominique PHAN <dphan.prestataire@oui.sncf>
2021-08-17 11:39:55 +0200

    Passage en KTS + Ajout des dépendances

---
+++ buildSrc/src/main/kotlin/projectconfig/BuildConfig.kt
@@ -0,0 +1,11 @@
+package projectconfig
+
+
+object BuildConfig {
+
+    object Versions {
+        const val KOTLIN_VERSION = "1.4.30"
+        const val ANDROID_BUILD_TOOLS_VERSION = "30.0.3"
+    }
+
+}
